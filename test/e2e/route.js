b32f63e6115657aaf032212675d0f4ba812beb82
Claudio Holanda <claudiohw@gmail.com>
2019-04-28 19:33:14 -0300

    Fixing redirect and adding more tests

--- test/e2e/route.js
+++ test/e2e/route.js
@@ -13,5 +13,22 @@ module.exports = {
       .waitForElementVisible('h1')
       .assert.containsText('h1', 'Hello from Index!')
       .end();
+  },
+  '<Route> Basic path (/{name}) Tests': (browser) => {
+    browser
+      // Checking if "/" Route loads Index Component
+      .url('http://localhost:5001/about')
+      .waitForElementVisible('body')
+      .waitForElementVisible('h1')
+      .assert.containsText('h1', 'Hello from About!')
+  },
+  '<Route> Redirect Attribute Tests': (browser) => {
+    browser
+      // Checking if "/" Route loads Index Component
+      .url('http://localhost:5001/company')
+      .pause(100)
+      .waitForElementVisible('h1')
+      .assert.containsText('h1', 'Hello from About!')
+      .assert.urlContains('/about');
   }
 };

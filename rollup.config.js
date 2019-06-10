54d23c4f6b6c747847a72365bfb7e58b040fca09
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-06-09 20:48:58 -0500

    Fixed build setup; dist to most targets

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,34 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
-const production = !process.env.ROLLUP_WATCH;
+const isDev = process.env.ROLLUP_WATCH;
+const isProd = process.env.NODE_ENV === 'production';
+
+function bundle(file, format) {
+  return {
+    sourcemap: false,
+    name: 'svero',
+    format,
+    file,
+  };
+}
 
 export default {
-	input: 'test/main.js',
-	output: {
-		sourcemap: false,
-		format: 'iife',
-		name: 'svero',
-		file: 'test/public/test.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-		}),
-		resolve(),
-		commonjs(),
-	]
+  input: isProd ? 'src/main.js' : 'test/main.js',
+  output: isProd ? [
+    bundle('dist/svero.js', 'cjs'),
+    bundle('dist/svero.es.js', 'es'),
+    bundle('dist/svero.min.js', 'umd'),
+  ] : bundle('test/public/test.js', 'iife'),
+  external: isProd ? ['svelte', 'svelte/store', 'svelte/internal'] : [],
+  plugins: [
+    svelte({
+      dev: isDev,
+    }),
+    resolve(),
+    commonjs(),
+    isProd && terser(),
+  ],
 };

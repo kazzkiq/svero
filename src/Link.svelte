ca7e11e4a8abba24c8d214d1894daae4d148ceff
Ali Almoullim <ali.almoullim@gmail.com>
2020-02-29 18:01:13 +0300

    Spread $$props into the 'a' tag instead of manually passing down each prop

--- src/Link.svelte
+++ src/Link.svelte
@@ -2,16 +2,7 @@
   import { onMount, createEventDispatcher } from 'svelte';
   import { navigateTo } from './utils';
 
-  let cssClass = '';
-
   export let href = '/';
-  export let className = '';
-  export let title = '';
-  export { cssClass as class };
-
-  onMount(() => {
-    className = className || cssClass;
-  });
 
   const dispatch = createEventDispatcher();
 
@@ -29,4 +20,6 @@
   }
 </script>
 
-<a {href} class={className} title={title} on:click|preventDefault={onClick}><slot></slot></a>
+<a {href} {...$$props} on:click|preventDefault={onClick}>
+  <slot></slot>
+</a>
